eb669f75e6b03d4ca723a1f11e7a1584b8e6ba37
scazochnik <vadimlevshin67@icloud.com>
2025-05-07 17:17:23 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+window.addEventListener("DOMContentLoaded", () => {
+  const btn = document.getElementById("test-btn");
+  btn.addEventListener("click", () => {
+    alert("🎉 Вы нажали кнопку! Очки начислены.");
+  });
+});
